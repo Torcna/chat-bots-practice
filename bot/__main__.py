fd78ccce51820ba361dc90c45cf5d29c46e86e95
ale.baranov <ale.baranov@yadro.com>
2025-10-13 20:27:44 +0300

    Task 3
    Vanilla python TG bot

---
+++ bot/__main__.py
@@ -0,0 +1,37 @@
+import time
+import bot.databaseClient
+import bot.tgClient
+
+
+def main() -> None:
+    offsetCounter = 0
+    try:
+        while True:
+            updates = bot.tgClient.getUpdates(offsetCounter)
+            bot.databaseClient.persist_updates(updates)
+            for upd in updates:
+                msg = upd.get("message")
+                if not msg:
+                    continue
+
+                chat_id = msg.get("chat", {}).get("id")
+                text = msg.get("text")
+
+                if text:
+                    print("there is text")
+                    bot.tgClient.sendMessage(chat_id=chat_id, text=text)
+                else:
+                    print("there is NO text")
+                    bot.tgClient.sendMessage(
+                        chat_id=chat_id,
+                        text="Cannot echo this type of messages, sr",
+                    )
+                print(".", end="", flush=True)
+                offsetCounter = max(offsetCounter, upd["update_id"] + 1)
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("\nStop")
+
+
+if __name__ == "__main__":
+    main()
---
+++ bot/tgClient.py
@@ -0,0 +1,40 @@
+import urllib.request
+import json
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def makeRequest(method: str, **params) -> dict:
+    jsonData = json.dumps(params).encode("utf-8")
+    print(f"{os.getenv('TELEGRAM_BASE_URI')}{os.getenv('TOKEN')}/{method}")
+    print(jsonData)
+    request = urllib.request.Request(
+        method="POST",
+        url=f"{os.getenv('TELEGRAM_BASE_URI')}{os.getenv('TOKEN')}/{method}",
+        data=jsonData,
+        headers={
+            "Content-Type": "application/json",
+        },
+    )
+    with urllib.request.urlopen(request) as response:
+        responseBody = response.read().decode("utf-8")
+        responseJSON = json.loads(responseBody)
+        if responseJSON["ok"] == True:
+            return responseJSON["result"]
+        else:
+            print("not ok")
+
+
+def getUpdates(offset: int) -> dict:
+    return makeRequest("getUpdates", offset=offset)
+
+
+def sendMessage(chat_id: int, text: str) -> dict:
+    return makeRequest("sendMessage", chat_id=chat_id, text=text)
+
+
+def getMe() -> dict:
+    return makeRequest("getMe")
